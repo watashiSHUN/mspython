8a5635cd6e69d2c7d12c7c248be1fc278db2321f
Shun Gram <mike53669@gmail.com>
2023-11-30 18:04:49 -0800

    auth server handles JWT

---
+++ auth/server.py
@@ -0,0 +1,77 @@
+import datetime
+import os
+
+import jwt
+from dotenv import load_dotenv
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+app = Flask(__name__)
+# connect to db, not starting db server
+# mysql://username:password@localhost/db_name
+mysql_user = os.environ.get("MYSQL_USER")
+mysql_password = os.environ.get("MYSQL_PASSWORD")
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"mysql://{mysql_user}:{mysql_password}@localhost/auth"
+db = SQLAlchemy(app)
+
+
+# TODO, use this model to build the DB instead of init.sql
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    # we can get authorization.username and authorization.password
+    auth = request.authorization
+    if not auth:
+        # if auth header does not exist
+        return "missing credentials", 401
+
+    # check db for username and password
+    user = User.query.filter_by(username=auth.username).first()
+    if not user or user.password != auth.password:
+        return "invalid credentials", 401
+
+    # authenticated against the server
+    # response is the JWT token, used for subsequent
+    # request instead of passing username and password everytime
+
+    # jwt is signed using the secret key
+    # NOTE: JWT can use sym key or asym key
+    # auth service can validate the token (when user send it back)
+
+    # TODO, read secret key from env
+    return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+
+
+def createJWT(username, secret, auth_permission):
+    return jwt.encode(
+        {
+            "username": username,
+            # expiration time needs to be a string
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+            "admin": auth_permission,
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host="0.0.0.0")
+    # docker container (like a machine) will have an IP address
+    # NOTE, with a docker network?
+
+    # IP address will locate a server
+    # => but we have many processes
+    # the docker container will have a process
